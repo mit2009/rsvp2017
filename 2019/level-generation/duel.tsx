1db2dfbadcdee6463c68b5ff6cc988f68548a1b8
poofytoo <victorhung92@gmail.com>
2019-12-05 15:18:57 -0500

    duel map

---
+++ 2019/level-generation/duel.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="tileeditor" tilewidth="30" tileheight="30" tilecount="12" columns="12">
+ <image source="../server/public/images/gameAssets/tileeditor.png" width="360" height="30"/>
+</tileset>
